92b0002462a803e77fb3eb0612c6b7ebd5f321fb
aspizu <aspizu@protonmail.com>
2024-02-18 17:28:34 +0530

    remove redirect to login

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,18 +1,6 @@
-import {useEffect} from "react"
-import {useNavigate} from "react-router-dom"
 import {Navbar} from "~/components/Navbar"
-import {fetchSession, session} from "~/globalState"
 
 export function Root() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        ;(async () => {
-            await fetchSession()
-            if (session.value == null) {
-                navigate("/login")
-            }
-        })()
-    }, [])
     return (
         <div>
             <Navbar />
