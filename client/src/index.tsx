c3f8566f8e37eb4e1d1f3afc5cf5c2e1a14194bd
aspizu <aspizu@protonmail.com>
2024-02-18 13:25:52 +0530

    NextUI and 404 Route

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,31 @@
+import {NextUIProvider} from "@nextui-org/react"
 import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
-import {App} from "~/App"
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import "~/index.css"
+import {PageNotFound} from "~/routes/PageNotFound"
+import {Root} from "~/routes/Root"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+    {
+        path: "*",
+        element: <PageNotFound />,
+    },
+])
+
+function Base() {
+    return (
+        <StrictMode>
+            <NextUIProvider>
+                <RouterProvider router={router} />
+            </NextUIProvider>
+        </StrictMode>
+    )
+}
 
 const root = createRoot(document.getElementById("app")!)
-root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-)
+root.render(<Base />)
