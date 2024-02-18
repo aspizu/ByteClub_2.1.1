3d27f3e789a754451fe5725953c100d46b463330
Tushar <tusharbadlani0@gmail.com>
2024-02-18 16:10:47 +0530

    startup routes added

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -30,8 +30,8 @@ class User(msgspec.Struct):
 reproca: Reproca[int, User] = Reproca()
 
 # You should import all modules which create reproca methods here.
-from . import blog, user, mentorship, mentor  # noqa: E402
-_ = (user, blog, mentorship, mentor)  
+from . import blog, user, mentorship, mentor, startup  # noqa: E402
+_ = (user, blog, mentorship, mentor, startup)  
 
 # This will generate API bindings for Typescript inside the client src directory.
 with Path("../client/src/api.ts").open("w") as file:
